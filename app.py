c84f09232e5541bd57a0e780e1353de1af727143
Dennis Peters <dennis95peters@gmail.com>
2021-06-15 18:00:13 +0300

    Test API functionality of the Free Currency Converter

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import requests
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    API = os.environ.get("CURR_API")
+    fromCurr = "USD"
+    toCurr = "KES"
+    url = f"https://free.currconv.com/api/v7/convert?q={fromCurr}_{toCurr}&compact=ultra&apiKey=385fcd73db41fdffd372"
+    rates = requests.get(url).json()
+    return rates
+
+if __name__ == "__main__":
+    app.run(debug=True)
