fec253500d8f9818fe5a417c59666340edf06f78
Dennis Peters <dennis95peters@gmail.com>
2021-06-15 19:41:00 +0300

    Create an input form to take in the currency to converted

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    API = os.environ.get("CURR_API")
+    """API = os.environ.get("CURR_API")
     fromCurr = "USD"
     toCurr = "KES"
     url = f"https://free.currconv.com/api/v7/convert?q={fromCurr}_{toCurr}&compact=ultra&apiKey=385fcd73db41fdffd372"
-    rates = requests.get(url).json()
-    return rates
+    rates = requests.get(url).json()"""
+    if request.method == "POST":
+        return ""
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
